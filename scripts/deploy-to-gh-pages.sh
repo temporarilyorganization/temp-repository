1ec61c57782c26a907512368350f4fb62e8bd8cf
Evgeniy Khaberev <ekhaberev@ics.perm.ru>
2019-04-19 02:51:18 +0500

    ci: try to fix deploy

--- scripts/deploy-to-gh-pages.sh
+++ scripts/deploy-to-gh-pages.sh
@@ -6,10 +6,10 @@ set -e
 echo "Pull request: ${TRAVIS_PULL_REQUEST}"
 echo "Parameter: ${1}"
 
-if [[ ${TRAVIS_PULL_REQUEST} == "false" ]]
+if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]
 then
   echo "Not PR"
-  if [[ ${1} == "master"]] || [[ ${1} == "develop" ]]
+  if [[ "${1}" == "master" ]] || [[ "${1}" == "develop" ]]
   then
     echo "Correct branch"
     # Define repository relative GitHub address.
