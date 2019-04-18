d9864467e63b8d67fec4be7275d1b30420b64b1f
Evgeniy Khaberev <ekhaberev@ics.perm.ru>
2019-04-19 02:19:35 +0500

    ci: testing deploy

---
+++ scripts/deploy-to-gh-pages.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+# Exit with nonzero exit code if anything fails.
+set -e
+
+echo "Pull request: ${TRAVIS_PULL_REQUEST}"
+
+if [[ ${TRAVIS_PULL_REQUEST} = "false"]] && [[ ${1} = "master" || ${1} = "develop" ]]
+then
+    # Define repository relative GitHub address.
+    repositoryRelativeGitHubAddress="temporarilyorganization/temp-repository"
+
+    # Clone project into 'repository' subdirectory && move to it.
+    echo "Prepare for deploy to gh-pages."
+    echo $PWD
+    echo "Clone ${repositoryRelativeGitHubAddress} repository & checkout latest version of gh-pages branch."
+    git clone --recursive "https://github.com/${repositoryRelativeGitHubAddress}.git" repo
+    cd repo
+    echo $PWD
+
+    # Checkout gh-pages branch & pull it's latest version.
+    git checkout gh-pages
+    git pull
+
+    # Remove results of previous deploy (for current branch) & recreate directory.
+    echo "Remove results of previous deploy (for ${TRAVIS_BRANCH} branch)."
+    rm -rf "${TRAVIS_BRANCH}"
+    mkdir "${TRAVIS_BRANCH}"
+
+    # Copy builded ember application from 'dist' directory into 'repository/${TRAVIS_BRANCH}'.
+    echo "Copy builded ember application (for ${TRAVIS_BRANCH} branch)."
+    cp -r ../src/* "${TRAVIS_BRANCH}"
+    cd "${TRAVIS_BRANCH}"
+    ls -l
+    cd ..
+fi
+
+
+# Configure git.
+#git config user.name "Flexberry-man"
+#git config user.email "mail@flexberry.net"
+
+#echo "Commit & push changes."
+#git add --all
+#git commit -m "Update gh-pages for ${TRAVIS_BRANCH} branch"
+
+# Redirect any output to /dev/null to hide any sensitive credential data that might otherwise be exposed.
+#git push --force --quiet "https://${GH_TOKEN}@github.com/${repositoryRelativeGitHubAddress}.git" > /dev/null 2>&1
+
+echo "Deploy to gh-pages finished."
